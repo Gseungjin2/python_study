5be504a9d58c240b051b8084efed90acf150f269
Gseungjin2 <jchsekfr5@gmail.com>
2025-05-22 23:32:49 +0900

    python_study_day7: 도건과제2 - 영상답안 확인

--- python_study_day7_hangman/step_1/solution.py
+++ python_study_day7_hangman/step_1/solution.py
@@ -17,10 +17,31 @@ for index, letter in enumerate(chosen_word):
 
 updated_dispay_holder = ''.join(dispay_holder)
 
-print(updated_dispay_holder)
+print(type(updated_dispay_holder))
         
 
+     
+'''
+강의 영상 답안
+'''
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
+placeholder = ""
+word_length = len(chosen_word)
 
-           
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
+guess = input("Guess a letter:").lower()
+
+display = ""
+
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+    else:
+        display += "_"
+
+print(display)
