0fb8bcb5847d5c0e0692eecabbafe2a00f07c525
Gseungjin2 <jchsekfr5@gmail.com>
2025-05-28 00:19:01 +0900

    python_study_day7: 도전과제 3

--- python_study_day7_hangman/step_1/solution.py
+++ python_study_day7_hangman/step_1/solution.py
@@ -1,58 +1,68 @@
 from main import word_list
 import random
 
-chosen_word = random.choice(word_list)
+# chosen_word = random.choice(word_list)
 
-print(chosen_word)
+# print(chosen_word)
 
-display_holder = ['_'] * len(chosen_word) 
+# display_holder = ['_'] * len(chosen_word) 
 
-print(''.join(display_holder))
+# print(''.join(display_holder))
 
-# TOOD-1: - Use a while loop to let the user again.
-gaem_over = False
+# # TOOD-1: - Use a while loop to let the user again.
+# gaem_over = False
 
-while not gaem_over:
-    guess = input("Guess a letter:").lower()
+# while not gaem_over:
+#     guess = input("Guess a letter:").lower()
     
 
-#TOOD-2: - Change the for loop so that you keep the previous correct guesses.
+# #TOOD-2: - Change the for loop so that you keep the previous correct guesses.
 
-for index, letter in enumerate(chosen_word):
-    if letter == guess:
-        display_holder[index] = letter
+# for index, letter in enumerate(chosen_word):
+#     if letter == guess:
+#         display_holder[index] = letter
 
-updated_dispay_holder = ''.join(display_holder)
+# updated_dispay_holder = ''.join(display_holder)
 
-print(updated_dispay_holder)
+# print(updated_dispay_holder)
         
 
      
 '''
 강의 영상 답안
 '''
-# chosen_word = random.choice(word_list)
-# print(chosen_word)
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# placeholder = ""
-# word_length = len(chosen_word)
-
-# for position in range(word_length):
-#     placeholder += "_"
-# print(placeholder)
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
 # TOOD-1: - Use a while loop to let the user again.
+game_over = False
+correct_letter = [] # 이전에 하나씩 입력 받은 문자열을 저장하려면 무조건 리스트로 만들어야한다.중요
 
-# guess = input("Guess a letter:").lower()
+while not game_over:
+    guess = input("Guess a letter:").lower()
 
-# display = ""
+    display = ""
 
-#TOOD-2: - Change the for loop so that you keep the previous correct guesses.
+    # TOOD-2: - Change the for loop so that you keep the previous correct guesses.
 
-# for letter in chosen_word:
-#     if letter == guess:
-#         display += letter
-#     else:
-#         display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
 
-# print(display)
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+        
